c86cacf596d623d4ea94c184a1bc1eeaadd23abb
Wesley Emeneker <wesley.emeneker.ctr@mhpcc.hpc.mil>
2018-03-21 16:56:28 -1000

    Refactoring, adding tests, and documenting.
    I've started this mostly to learn this codebase to help solve
      a problem of being able to use all GPUs on a node instead
      of just one at a time.

--- pyhpc/cluster_experiment.py
+++ pyhpc/cluster_experiment.py
@@ -1,6 +1,7 @@
-
-#!/usr/bin/python
+#!/usr/bin/env python
 # Example PBS cluster job submission in Python
+
+from __future__ import print_function
 
 import os
 import csv
@@ -9,11 +10,29 @@ import random
 import itertools
 import subprocess
 
+JOB_SCRIPT_TEMPLATE = """\
+#!/bin/bash
+#PBS -N {job_name}
+#PBS -l walltime={walltime}
+#PBS -l select={select}
+#PBS -o ~/log/output/{job_name}.out
+#PBS -e ~/log/error/{job_name}.err
+#PBS -A {account}
+#PBS -q {queue}
+cd $PBS_O_WORKDIR
+{command_prologue}
+{command}
+{command_epilogue}
+"""
+
 
 class ClusterExperiment(object):
-
+    account = ''
+    queue = 'standard'
+    walltime = '4:00:00'
+    resources = '1:ncpus=20:mpiprocs=20'
+    
     def __init__(self):
-
         self.experimental_configs = []
         self.independent_designs = []
         self.coupled_designs = []
@@ -21,134 +40,98 @@ class ClusterExperiment(object):
         self._input_output_maps = []
         self._job_ids = []
 
-    def add_design(self, flag, value_list):
+    def add_design(self, flag, value_it):
+        """Add a range of values for the experiment with the range of values.
 
-        self.independent_designs.append((flag, value_list))
+        Parameters
+        ----------
+        flag (str) - 
+        """
+        self.independent_designs.append((flag, list(value_it)))
 
     def add_coupled_design(self, coupled_design):
-
         self.coupled_designs.append(coupled_design)
 
     def set_rep_count(self, num_reps):
-
         self.add_design('rep_num', range(num_reps))
 
     def get_configs(self):
-
         # Translate the design structure into flag strings.
-        exp_flag_strings = [['--' + f + '=' + str(v) for v in r]
-                            for (f, r) in self.independent_designs]
+        exp_flag_strings = [['--{}={}'.format(f, v) for v in r] for (f, r) in self.independent_designs]
 
         # Produce the Cartesian set of configurations.
         indep_experimental_configs = list(itertools.product(*exp_flag_strings))
-
-        # Initialize a list to store coupled configurations.
         coupled_configs = []
-
         # Scope this variable higher due to write-out coupling.
         coupled_flag_strs = []
 
         for coupled_design in self.coupled_designs:
-
             for d in coupled_design:
-
-                coupled_flag_strs = [['--' + f + '=' + str(v) for v in r]
-                                     for (f, r) in d]
-
+                coupled_flag_strs = [['--{}={}'.format(f, v) for v in r] for (f, r) in d]
                 coupled_configs += list(itertools.product(*coupled_flag_strs))
 
-        # Initialize empty experimental configs list...
-        experimental_configs = []
-
-        # ...and if there are coupled configs...
+        # Join each coupled config to each independent config
         if coupled_configs:
-
-            # ...iterate over each independent config...
+            experimental_configs = []
             for e in indep_experimental_configs:
+                for c in coupled_configs:
+                    experimental_configs.append(e + tuple(c))
 
-                    # ...then for each coupled config...
-                    for c in coupled_configs:
-
-                        # ...join the coupled config to the independent one.
-                        experimental_configs.append(e + tuple(c))
-
-        # Otherwise, ....
         else:
-
-            # ...just pass the independent experiments through.
             experimental_configs = indep_experimental_configs
 
-        return(experimental_configs)
+        return experimental_configs
 
     def get_parameter_labels(self):
-
-        parameter_labels = []
-
-        for (f, _) in self.independent_designs:
-
-            parameter_labels.append(f)
+        parameter_labels = [f for (f, _) in self.independent_designs]
 
         for coupled_design in self.coupled_designs:
-
             coupled_flag_strs = []
-
-            for d in coupled_design:
-
-                coupled_flag_strs = [f for (f, _) in d]
-
-            parameter_labels += coupled_flag_strs
+            parameter_labels.extend([f for (f, _) in d for d in coupled_design])
 
         return(parameter_labels)
 
-    def launch_experiment(self,
-                          exp_filename,
-                          log_dir,
-                          manager='pbs',
-                          shuffle_job_order=True):
+    def launch_experiment(self, exp_filename, log_dir,
+                          manager='pbs', shuffle_job_order=True, job_fmt='dist_ex_{}',
+                          walltime=None, select=None,
+                          account=None, queue=None):
 
+        if account is None:
+            account = self.account
+
+        if queue is None:
+            queue = self.queue
+
+        if walltime is None:
+            walltime = self.walltime
+
+        if select is None:
+            select = self.resources
+            
         experimental_configs = self.get_configs()
 
         if shuffle_job_order:
-
             # Shuffle the submission order of configs to avoid asymetries.
             random.shuffle(experimental_configs)
 
         # Iterate over each experimental configuration, launching jobs.
         for i, experimental_config in enumerate(experimental_configs):
-
-            if manager == 'pbs':
-
-                # Some prints for easy debug.
-                # print("-----------experimental_config---------")
-                # print(experimental_config)
-                # print("---------------------------------------")
-
+            if manager.lower() == 'pbs':
                 # Use subproces to command qsub to submit a job.
-                p = subprocess.Popen('qsub',
-                                     stdin=subprocess.PIPE,
-                                     stdout=subprocess.PIPE,
-                                     shell=True)
+                p = subprocess.Popen('qsub', stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
 
                 # Customize your options here.
-                job_name = "dist_ex_%d" % i
-                walltime = "4:00:00"
-                select = "1:ncpus=20:mpiprocs=20"
-                command = "python " + exp_filename
-
-                # Iterate over flag strings, building the command.
-                for flag in experimental_config:
-
-                    command += ' ' + flag
-
-                # Add a final flag modifying the log filename to be unique.
-                log_filename = 'templog' + str(i)
-
-                temp_log_dir = log_dir + 'templog' + str(i) + '/'
-
-                command += ' --log_dir=' + temp_log_dir
-
-                # Add the logfile to the command.
-                command += ' --log_filename=' + log_filename
+                job_name = job_fmt.format(i)
+                temp_log_dir = os.path.join(log_dir, 'templog_' + str(i))
+                log_filename = 'templog_' + str(i)
+                
+                command_prologue = "module load anaconda2/5.0.1 gcc/5.3.0 cudnn/6.0"
+                command_epilogue = ""
+                command = "python {filename} {config} --log_dir={log_dir} --log_filename={log_filename}"
+                command = command.format(filename=exp_filename,
+                                         config=' '.join(experimental_config),
+                                         log_dir=temp_log_dir,
+                                         log_filename=log_filename)
 
                 # Build IO maps.
                 input_output_map = (experimental_config,
@@ -157,38 +140,27 @@ class ClusterExperiment(object):
 
                 self._input_output_maps.append(input_output_map)
 
-                # qsub -I -X -l walltime=0:30:00 -l select=1:ncpus=20:mpiprocs=20 -l place=scatter:excl -A MHPCC96670DA1 -q debug -V
-                # Build the job string.
-                job_string = """#!/bin/bash
-                #PBS -N %s
-                #PBS -l walltime=%s
-                #PBS -l select=%s
-                #PBS -o ~/log/output/%s.out
-                #PBS -e ~/log/error/%s.err
-                #PBS -A MHPCC96670DA1
-                #PBS -q standard
-                module load anaconda2/5.0.1 gcc/5.3.0 cudnn/6.0
-                cd $PBS_O_WORKDIR
-                %s""" % (job_name,
-                         walltime,
-                         select,
-                         job_name,
-                         job_name,
-                         command)
+                job_script = JOB_SCRIPT_TEMPLATE.format(job_name=job_name,
+                                                        walltime=walltime,
+                                                        select=select,
+                                                        command=command,
+                                                        account=account,
+                                                        queue=queue,
+                                                        command_prologue=command_prologue,
+                                                        command_epilogue=command_epilogue)
 
-                # Print your job string.
-                print(job_string)
+                print(job_script)
 
                 # Send job_string to qsub.
-                self._job_ids.append(p.communicate(job_string)[0])
+                self._job_ids.append(p.communicate(job_script)[0])
                 time.sleep(1)
 
             else:
+                raise ValueError("Unknown manager '{}' supplied to launch_experiment(). Allowed managers: 'pbs'".format(manager))
 
-                print("Unknown manager supplied to launch_experiment().")
-                exit()
-
-    def join_job_output(self, log_dir, log_filename, max_runtime):
+    def join_job_output(self, log_dir, log_filename, max_runtime, job_ids):
+        '''Wait until all the the jobs complete
+        '''
 
         jobs_complete = False
         timeout = False
@@ -196,23 +168,16 @@ class ClusterExperiment(object):
 
         # Loop until timeout or all jobs complete.
         while not(jobs_complete) and not(timeout):
-
             print("-----------------")
-
             print('Time elapsed: ' + str(elapsed_time) + ' seconds.')
-
             time.sleep(10)
-
             elapsed_time += 10
 
             # Create a list to hold the Bool job complete flags
             job_complete_flags = []
-
             # Iterate over each job id string.
             for job_id in self._job_ids:
-
                 # TODO: Handle job completion gracefully.
-
                 # Issue qstat command to get job status.
                 p = subprocess.Popen('qstat -r ' + job_id,
                                      stdin=subprocess.PIPE,
@@ -224,19 +189,15 @@ class ClusterExperiment(object):
                 try:
                     # Read the qstat out, parse the state, and conv to Boolean.
                     job_complete = output[0].split()[-2] == 'E'
-
                 except:
-
                     job_complete = True
 
                 # Print a diagnostic.
-                print('Job ' + job_id[:-1] + ' complete? ' +
-                      str(job_complete) + '.')
+                print('Job', job_id[:-1], 'complete?', str(job_complete))
 
                 job_complete_flags.append(job_complete)
 
                 if job_complete:
-
                     p = subprocess.Popen('qdel -Wforce ' + job_id,
                                          stdin=subprocess.PIPE,
                                          stdout=subprocess.PIPE,
@@ -253,7 +214,6 @@ class ClusterExperiment(object):
 
             # Accomodate Python 2.7 on Hokulea.
             with open(log_dir + '/' + log_filename, 'wb') as csvfile:
-
                 # Manually note response varaibles.
                 response_labels = ['step_num',
                                    'train_loss',
@@ -278,30 +238,21 @@ class ClusterExperiment(object):
 
                     # Process the flags into output values.
                     for flag in input_flags:
-
                         flag_val = flag.split('=')[1]
-
                         input_row.append(flag_val)
 
                     output_file = output_dir + output_filename
-
                     print("output_file")
 
                     # Check if the output file has been written.
                     if os.path.exists(output_file):
-
                         with open(output_file, 'rb') as f:
-
                             reader = csv.reader(f)
-
                             for output_row in reader:
-
                                 csvwriter.writerow(input_row + output_row)
-
                         print("---------------------------------------")
 
                     else:
-
                         print("output filename not found: " + output_filename)
 
             print("-----------------")
---
+++ tests/test_launch_experiment.py
@@ -0,0 +1,43 @@
+
+import os
+
+import pytest
+import mock
+
+from pyhpc import cluster_experiment
+
+
+def test_launch_experiment_01():
+    '''Check that launching an experiment sends a correct job script to PBS'''
+    experiment = cluster_experiment.ClusterExperiment()
+    experiment.add_design('parameter', [1])
+    
+    with mock.patch('pyhpc.cluster_experiment.subprocess.Popen') as Popen:
+        main_dir = os.path.join('home', 'pyhpc')
+        experiment.launch_experiment('experiment.py', main_dir, shuffle_job_order=False)
+
+        script_template = cluster_experiment.JOB_SCRIPT_TEMPLATE
+        expected_command = 'python experiment.py --parameter=1 --log_dir={} --log_filename={}'.format(os.path.join(main_dir, 'templog_0'), 'templog_0')
+        expected_prologue = "module load anaconda2/5.0.1 gcc/5.3.0 cudnn/6.0"
+        expected_epilogue = ''
+        expected_script = script_template.format(job_name='dist_ex_0',
+                                                 walltime='4:00:00',
+                                                 select='1:ncpus=20:mpiprocs=20',
+                                                 command=expected_command,
+                                                 command_prologue=expected_prologue,
+                                                 command_epilogue=expected_epilogue,
+                                                 account=experiment.account,
+                                                 queue=experiment.queue)
+        assert Popen.mock_calls[1][1][0] == expected_script
+
+
+def test_non_pbs_manager():
+    '''Check that when a non PBS queue manager is requested, that experiment launching fails.
+
+    As of 2018-03-21, only PBS is supported.
+    '''
+    experiment = cluster_experiment.ClusterExperiment()
+    experiment.add_design('parameter', [1])
+
+    with pytest.raises(ValueError):
+        experiment.launch_experiment('experiment.py', '/tmp', manager='LSF')
